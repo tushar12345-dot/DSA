3baafbb0413dfd166d3e43bee14c045de31f3bff
Tushar Sharma <tusharsharma5211@gmail.com>
2025-01-07 18:09:35 +0530

    Reverse String

--- Example.java
+++ Example.java
@@ -2,12 +2,15 @@ class Example{
 public static void main(String[]args){
 Reverse("abcde");
 }
-public static void Reverse(String s){
+public static void Reverse(String s)
+{
 String r="";
 char ch;
-for(int i=0;i<s.length();i++){ 
-//Extract each character from the string 
+for(int i=0;s.length();i++)
+{
+//Extract each character from given string
 ch=s.charAt(i);
+//Add character one by one 
 r=ch+r;
 }
 System.out.println(r);
